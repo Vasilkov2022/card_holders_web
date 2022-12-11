fff18989164b2c9134e8e7fe8f39761deb127ada
Vasilkov2022 <vasilkovandr249@gmail.com>
2022-12-11 17:15:13 +0300

    requests

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import seaborn as sns
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 data = pd.read_csv("/Users/andrei_vasilkov/hse_dataset_project_december/BankChurners.csv")
 # print(data)
@@ -13,9 +13,9 @@ st.write(data.head())
 st.write('This chart shows numbers of existing customers of specific age')
 st.bar_chart(data[(data.Attrition_Flag == 'Existing Customer')]['Customer_Age'].value_counts())
 
-fig = plt.figure(figsize=(10, 4))
-sns.boxplot(data=data, x='Education_Level', y='Credit_Limit')
-st.pyplot(fig)
+# fig = plt.figure(figsize=(10, 4))
+# sns.boxplot(data=data, x='Education_Level', y='Credit_Limit')
+# st.pyplot(fig)
 
 fig = px.scatter(data, x="Customer_Age", y="Credit_Limit", color="Attrition_Flag",
                  marginal_x="box", marginal_y="violin",
